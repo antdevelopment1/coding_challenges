a3dc3321d4ba85ab3feb8d877b8ffbd55dea16c3
April Copes <antdevelopment1@gmail.com>
2018-10-03 21:56:22 -0400

    Creates a function that adds one to the argument

--- coding_challenges.js
+++ coding_challenges.js
@@ -24,3 +24,10 @@ function add(a, b) {
     return a + b;
 }
 console.log(add(10, 20));
+
+// Create a function that takes a number as an argument, 
+// increments the number by +1 and returns the result.
+function increment(num) {
+    return num + 1;
+}
+console.log(increment(10));
