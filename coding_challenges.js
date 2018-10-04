c08e24c551a9d67a6e48b078a9967df8aaa5177e
April Copes <antdevelopment1@gmail.com>
2018-10-03 21:54:27 -0400

    create a function that adds two numbers

--- coding_challenges.js
+++ coding_challenges.js
@@ -17,3 +17,10 @@ function reverse(str) {
     return str.split("").reverse().join("");
 }
 console.log(reverse("April Copes"));
+
+// Create a function that takes two numbers as 
+// arguments and return their sum.
+function add(a, b) {
+    return a + b;
+}
+console.log(add(10, 20));
