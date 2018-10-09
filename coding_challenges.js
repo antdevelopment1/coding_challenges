1105da1a83083225512df1843232f3502a3da042
April Copes <antdevelopment1@gmail.com>
2018-10-08 20:21:27 -0400

    Creates a function that multiplies first value passed by the second value

--- coding_challenges.js
+++ coding_challenges.js
@@ -204,3 +204,16 @@ function isAvgWhole(arr) {
 	}	
 }
 
+// Create a function that takes two arguments (item, times). The first argument (item) is the item that needs 
+// repeating while the second argument (times) is the number of times the item is to be repeated. Return the result in an array.
+
+function repeat(item, times) {
+	var newStr = [];
+	count = 0;
+	while (count < times) {
+		newStr.push(item);
+		count++;
+	}
+	return newStr;
+}
+
