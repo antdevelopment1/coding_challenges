a94a816de5c9a4a6a40059e623c0f5b6d06db0a2
April Copes <antdevelopment1@gmail.com>
2018-10-11 21:06:56 -0400

    Creates a function that gets the date to see if its Christmas Eve.

--- easy_coding_challenges.js
+++ easy_coding_challenges.js
@@ -22,3 +22,16 @@ function filterArray(arr) {
     })
     return list;
 }
+
+// Christmas Eve is almost upon us, so naturally we need to prepare some milk and cookies for Santa! Create a function that accepts a Date object and returns true if it's Christmas Eve 
+// (December 24th) and false otherwise. Keep in mind JavaScript's Date month is 0 based, meaning December is the 11th month while January is 0.
+
+function timeForMilkAndCookies(date) {
+	var day = date.getDate();
+  var month = date.getMonth();
+	if (day === 24 && month === 11) {
+		return true;
+	} else {
+		return false;
+	}
+}
