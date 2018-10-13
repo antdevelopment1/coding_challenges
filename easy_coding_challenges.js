da17f711e7d77ae0202fda53a874226b1e5b490a
April Copes <antdevelopment1@gmail.com>
2018-10-12 23:56:25 -0400

    Creates a function that returns an array with max values of array of arrays

--- easy_coding_challenges.js
+++ easy_coding_challenges.js
@@ -133,3 +133,15 @@ function sortNumsAscending(arr) {
 	}
 	return sorted;
 }
+
+// Create a function that takes an array of arrays with numbers. Return a new (single) array with the largest numbers of each.
+function findLargestNums(arr) {
+	var emptyArr = [];
+	for (var i = 0; i < arr.length; i++) {
+		var maxVal = arr[i].sort(function(a, b){
+		return b - a;
+		});
+		emptyArr.push(maxVal[0]);
+		}
+	return emptyArr;
+}
