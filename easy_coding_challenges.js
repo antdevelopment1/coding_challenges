fbaf70a5756490ffd502def6ba2a6f2a21ee3cfc
April Copes <antdevelopment1@gmail.com>
2018-10-12 22:53:50 -0400

    Creates a function returns a new array in ascending order

--- easy_coding_challenges.js
+++ easy_coding_challenges.js
@@ -119,3 +119,17 @@ function verifySubstrs(mainStr, head, body, tail) {
 		return "Incomplete.";
 	}
 }
+
+// Create a function that takes an array of numbers and returns a new array, sorted in ascending order (smallest to biggest).
+// Sort numbers array in ascending order. If functions argument is null, an empty array or undefined, return an empty array. Return new array of sorted numbers.
+
+function sortNumsAscending(arr) {
+	if (arr === null || arr === [] || arr === undefined) {
+		return [];
+	} else {
+		var sorted = arr.sort(function(a, b) {
+			return a - b;
+		})
+	}
+	return sorted;
+}
